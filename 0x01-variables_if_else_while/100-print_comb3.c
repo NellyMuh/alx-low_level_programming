7693f10873bb644ac00cef5e3e8a627bca4d70ef
NellyMuh <m.nelly704@gmail.com>
2022-10-14 18:57:50 -0700

    files created

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+	while (j < 58)
+	{
+		putchar(j);
+		putchar(i);
+		if (i == 57 && j == 57)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	j++;
+}
+	putchar('\n');
+	return (0);
+}
