dde019d0078705f239a9a84334d7607487e96cfc
NellyMuh <m.nelly704@gmail.com>
2022-10-18 14:38:09 -0700

    added file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int a, b;
+	int i;
+	char c;
 
-	for (a = 0; a <= 9; a++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (b = 97; b <= 122; b++)
+		for (c == 'a' || c == 'z')
 		{
-			_putchar(b);
+			_putchar(c);
 		}
 	_putchar('\n');
 	}
