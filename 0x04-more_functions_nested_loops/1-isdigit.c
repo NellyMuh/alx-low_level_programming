2d727ba4fe7c525573ab8d61c92355645bd3f23f
NellyMuh <m.nelly704@gmail.com>
2022-10-20 15:00:30 -0700

    modified file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,12 +4,12 @@
  * _isdigit - returns 1 if c is a digit
  * @c: checks character
  *
- * Return: 0 
+ * Return: 0
  **/
 
 int _isdigit(int c)
 {
-	if( c >= '1' && c <= '10')
+	if (c >= '1' && c <= '10')
 		return (1);
 	else
 		return (0);
