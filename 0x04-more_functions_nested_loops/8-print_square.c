1299c01a5295de559d21930c7aebe676419d1bbc
NellyMuh <m.nelly704@gmail.com>
2022-10-21 12:26:52 -0700

    created file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,15 @@
 
 void print_square(int size)
 {
-	int i;
+	int i, c;
+
+	if (size <= 0)
+		_putchar('\n');
 
 	for (i = 0; i <= size; i++)
 	{
 		for (c = 0; c < size; c++)
 		{
-		if (size <= 0)
-			_putchar('\n');
-		else
 			_putchar('#');
 		}
 	}
