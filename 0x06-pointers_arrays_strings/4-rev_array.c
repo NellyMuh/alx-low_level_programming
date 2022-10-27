38dac207e99e53aa164c24a1482d9731f6ea5509
NellyMuh <m.nelly704@gmail.com>
2022-10-27 12:51:27 -0700

    created file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * rev_aray - prints an array in reverse
+ *@a: array of integers.
+ *@n: the number of elements to swap
+ * Retun : Always success
+ **/
+
+void reverse_array(int *a, int n)
+{
+	int i, a[];
+	
+	for ( i = n-1; i > 0; i--)
+	{
+		printf("the reverse array is: %d\n", a[i]);
+	}	
+	return (0);
+}
