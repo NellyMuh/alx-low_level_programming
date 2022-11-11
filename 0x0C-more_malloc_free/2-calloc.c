8a5aa69cf167f5463e1ab7b54437541ec2cd5c55
NellyMuh <m.nelly704@gmail.com>
2022-11-11 14:37:36 -0800

    edited file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,8 +18,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	p = malloc(size * nmemb);
 	if (p == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < nmemb * size; i += 1)
+	}
+	for (i = 0; i < nmemb * size; i ++)
+	{
 		*(p + i) = 0;
+	}
 	return (p);
 }
