7ec4223e352076bdb548ad66fe42e16d55711b00
NellyMuh <m.nelly704@gmail.com>
2022-11-16 08:30:51 -0800

    added file

---
+++ 0x0F-functions_pointers/functions_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_Pointers
+#define FUNCTIONS_POINTERS
+
+
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+
+
+
+
+#endif
